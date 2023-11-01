d9bef93d7e87ee31c1cc743fa7664d14a1a68b91
Theo Sementa <kaayzenn7@gmail.com>
2023-11-01 10:55:14 +0100

    Initial Commit

---
+++ CornerPadding/CornerPaddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerPaddingApp.swift
+//  CornerPadding
+//
+//  Created by KaayZenn on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CornerPaddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
